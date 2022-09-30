8b3667b9bbc597ec9ce0e4ee9b7c452f40ea83d7
Emil Folino <emilfolino@gmail.com>
2022-09-30 11:05:50 +0200

    29918-kmom06

---
+++ menu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Function that print out the choices in menu
+"""
+
+def menu():
+    """
+    The Menu
+    """
+    print(
+        """
+        lines) Count lines")
+        words) Count words")
+        letters) Count letters")
+        word_frequency) Find 7 most used words")
+        letter_frequency) Find 7 most used letters")
+        all) Do everything")
+        change) Change file")
+        q) quit")
+        """
+        )
