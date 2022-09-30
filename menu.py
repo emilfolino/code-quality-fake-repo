2ab918a5e64cff77cf74ee7749988a6f94d7520b
Emil Folino <emilfolino@gmail.com>
2022-09-30 10:52:56 +0200

    29505-kmom06

---
+++ menu.py
@@ -0,0 +1,16 @@
+"""
+Meny Valet För Main.py
+"""
+def menu():
+    """
+    Menu Options
+    """
+    print("lines) Count Lines")
+    print("words) Count Words")
+    print("letters) Count Letters")
+    print("word_frequenzy Find 7 most used words")
+    print("letter_frequenzy Find 7 most used letters")
+    print("all) Do Everything!")
+    print("change) Change File")         
+    print("(q / Q ) - Quit")
+    return menu
