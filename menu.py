b20867d5ebfdb60ae4ddf9bc5bbab84f03e10db7
Emil Folino <emilfolino@gmail.com>
2022-09-30 10:50:58 +0200

    24124-kmom06

---
+++ menu.py
@@ -0,0 +1,10 @@
+#pylint: disable-all
+
+"""
+menyn som ska skrivas ut
+"""
+def printMenu():
+    """
+    Menyn som ska skrivas ut
+    """
+    print("lines)Count lines\nwords) Count words\nletters) Count letters\nword_frequency) Find 7 most used words\nletter_frequency) Find 7 most used letters\nall) Do everything\nchange) Change file\nq) quit.")
