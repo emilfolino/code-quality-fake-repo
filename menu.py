91e10b7a28330649eca3c77272e6bb796987f8c8
Emil Folino <emilfolino@gmail.com>
2022-09-30 11:04:40 +0200

    29885-kmom06

---
+++ menu.py
@@ -0,0 +1,19 @@
+"""
+Menu for analyzer
+"""
+
+def the_menu():
+    """
+    menu for analyzer
+    """
+    print("Welcome to the Analyzer")
+    print("""You can choose from:
+words
+lines
+letters
+all
+menu
+word_frequency
+letter_frequency
+q to Quit
+Change""")
