6795b26043a1e07da05f4da81a59d2a5b01df3f5
Emil Folino <emilfolino@gmail.com>
2022-09-30 11:11:19 +0200

    30137-kmom06

---
+++ menu.py
@@ -0,0 +1,15 @@
+"""Module for the counting of frequency"""
+def menu(_):
+    """the menu for analyzer"""
+    
+    print("")
+    print("lines) Count lines")
+    print("words) Count words")
+    print("letters) Count letters")
+    print("word frequency) Find 7 most used words")
+    print("letter frequency) Find 7 most used letters")
+    print("all) Do everything")
+    print("change) Change file")
+    print("q) Quit")
+    print("")
+    
