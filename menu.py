5b1ececb6ce3423528ec07986ec2f1a23d89bfab
Emil Folino <emilfolino@gmail.com>
2023-01-30 14:01:55 +0100

    30047-kmom06

---
+++ menu.py
@@ -0,0 +1,19 @@
+"""
+Menu function for main.py
+"""
+
+def print_menu():
+    """
+    Prints out the menu
+    """
+    print("\nMENU")
+    print("-----------------------------------")
+    print("'lines'")
+    print("'words'")
+    print("'letters'")
+    print("'word_frequency'")
+    print("'letter_frequency'")
+    print("'all'")
+    print("'change'")
+    print("'q'")
+    print("-----------------------------------")
