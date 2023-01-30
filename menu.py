962f2aec6705c19871ca69da7a5ea7395167a505
Emil Folino <emilfolino@gmail.com>
2023-01-30 16:07:46 +0100

    30794-kmom06

---
+++ menu.py
@@ -0,0 +1,18 @@
+"""
+this is the menu for the analyzer
+"""
+
+def showMenu():
+    """
+    prints the menu
+    """
+    print()
+    print("lines) Count the non-empty lines")
+    print("words) Count words")
+    print("letters) Count letters")
+    print("word_frequency) Print 7 most-used words")
+    print("letter_frequency) Print 7 most-used letters")
+    print("all) print all the information at once")
+    print("change) change the source text")
+    print("q) quit")
+    print()
