a6cad0852281a556572084d6fe775db03a5b9265
Emil Folino <emilfolino@gmail.com>
2023-01-30 15:02:48 +0100

    30325-kmom06

---
+++ menu.py
@@ -0,0 +1,17 @@
+"""Prints the menu for the analyzer"""
+
+def print_menu():
+    """Prints the menu"""
+
+    menu = """
+    lines) Count lines
+    words) Count words
+    letters) Count letters
+    word_frequency) Find 7 most used words
+    letter_frequency) Find 7 most used letters
+    all) Do everything
+    change) Change file
+    q) Quit
+
+    """
+    print(menu)
