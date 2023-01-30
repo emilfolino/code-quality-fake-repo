c9e36c46b67afd452916da2255bfb3d171a1e498
Emil Folino <emilfolino@gmail.com>
2023-01-30 11:52:59 +0100

    29744-kmom06

--- main.py
+++ main.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-Main for the marvin chatbot.
+Main for the text analysis program
 """
 
-import marvin
-import emission_functions
-import inventory
-
-
+import analyzer
+import menu
 
 """
 Its an eternal loop, until q is pressed.
@@ -17,99 +14,59 @@ It should check the choice done by the user and call a appropriate
 function.
 """
 def main():
-    """Main method containing structure for program Marvin"""
+    """Main method containing structure for program"""
 
-    # My inventory list
-    inventoryStatus = []
+    filename = "phil.txt"
+    with open(filename) as filehandle:
+        phil = filehandle.readlines()
 
     while True:
 
-        choice = marvin.menu_choice()
-        words = choice.split()
+        choice = menu.menu_choice()
 
         if choice == "q":
-            marvin.choice_q()
+            analyzer.choice_q()
             break
 
-        elif choice == "1":
-            marvin.greet()
+        elif choice == "lines":
+            numberNonEmptyLines = analyzer.lines(phil)
+            print("Number of non-empty lines in {txtname} is equal to {numberRows}".
+            format(txtname = filename, numberRows = numberNonEmptyLines))
 
-        elif choice == "2":
-            marvin.celcius_to_farenheit()
+        elif choice == "words":
+            numberWords = analyzer.words(phil)
+            print("Number of words in {txtname} is equal to {words}".
+            format(txtname = filename, words = numberWords))
 
-        elif choice == "3":
-            marvin.word_manipulation()
+        elif choice == "letters":
+            numberLetters = analyzer.letters(phil)
+            print("Number of letters in {txtname} is equal to {letters}".
+            format(txtname = filename, letters = numberLetters))
 
-        elif choice == "4":
-            marvin.sum_and_average()
+        elif choice == "word_frequency":
+            wordFreq = analyzer.word_frequency(phil)
+            print(wordFreq)
 
-        elif choice == "5":
-            marvin.hyphen_string()
+        elif choice == "letter_frequency":
+            letterFreq = analyzer.letter_frequency(phil)
+            print(letterFreq)
 
-        elif choice == "6":
-            marvin.is_isogram()
+        elif choice == "all":
+            allText = analyzer.allFunc(phil)
+            print(allText)
 
-        elif choice == "7":
-            marvin.compare_numbers()
-
-        elif choice == "8":
-            string = input("Enter a string to randomize: ")
-            print(marvin.randomize_string(string))
-
-        elif choice == "9":
-            string = input("Enter a string to get akronym: ")
-            outputAcronym = marvin.get_acronym(string)
-            print(outputAcronym)
-
-        elif choice == "10":
-            string = input("Enter a string to mask it: ")
-            outputMasked = marvin.mask_string(string)
-            print(outputMasked)
-
-        elif choice == "11":
-            string1 = input("Enter a string to search in: ")
-            string2 = input("Enter the value to find index of: ")
-            indexes = marvin.find_all_indexes(string1, string2)
-            print(indexes)
-
-        elif choice == "12":
-            search_word = input("Search for available data keys type search key word: ")
-            try:
-                countrySearchList = emission_functions.search_country(search_word)
-            except ValueError:
-                print("Country does not exist!")
+        elif choice == "change":
+            philOld = phil
+            phil = analyzer.change(phil)
+            if philOld != phil:
+                print("\nFile has been changed")
             else:
-                print("Following countries were found: " + ", ".join(countrySearchList))
-
-        elif choice == "13":
-            search_word = input("Enter country, year and year: ")
-            try:
-                [country, year1, year2] = search_word.split(",")
-            except ValueError:
-                print("Wrong input type!")
-            else:
-                try:
-                    emissionChange = emission_functions.get_country_change_for_years(country, year1, year2)
-                except KeyError:
-                    print("Wrong country!")
-                except ValueError:
-                    print("Wrong year!")
-                else:
-                    print(country + ":" +  str(emissionChange) + "%")
-
-        elif choice == "14":
-            countryInput = input("Enter country: ")
-            data = emission_functions.get_country_data(countryInput)
-            emission_functions.print_country_data(data)
-
-
-        elif "inv" in words:
-            inventory.run(words, inventoryStatus)
+                print("\nFile has not been changed")
 
         else:
-            marvin.not_valid()
+            analyzer.not_valid()
 
-        marvin.continue_func()
+        analyzer.continue_func()
 
 if __name__ == "__main__":
     main()
