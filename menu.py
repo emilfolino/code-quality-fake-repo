914f5e867368af2fbf6e13404658209c30915acf
Emil Folino <emilfolino@gmail.com>
2023-01-30 11:48:55 +0100

    29733-kmom06

---
+++ menu.py
@@ -0,0 +1,17 @@
+"""
+Displays text analyzer menu options.
+"""
+
+def menu():
+    """
+    Menu options for text analyzer.
+    """
+    print("\nANALYZER COMMANDS:")
+    print("lines)\t\t\tcount number of lines in file, excluding blank lines")
+    print("words)\t\t\tcount number of words in file")
+    print("letters)\t\tcount number of letters in file")
+    print("word_frequency)\t\tprint list of most frequent words")
+    print("letter_frequency)\tprint list of most frequent letters")
+    print("all)\t\t\trun every text analysis command")
+    print("q)\t\t\tquit")
+    print()
