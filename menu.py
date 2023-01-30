c97f5085dbe84dd0632095edb6a86fe38c639fda
Emil Folino <emilfolino@gmail.com>
2023-01-30 11:44:51 +0100

    29725-kmom06

---
+++ menu.py
@@ -0,0 +1,20 @@
+"""
+Menu code
+"""
+
+def printMenu():
+    """
+    Prints the menu
+    """
+    menu = r"""
+    Welcome to the word analyzer. What do you wish to do?
+    lines
+    words
+    letters
+    word_frequency
+    letter_frequency
+    all
+    change
+    Press q to quit.
+    """
+    print(menu)
