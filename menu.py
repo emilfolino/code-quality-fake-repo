124aeaa95cf34808564f560dcfabcbe2e679ecb2
Emil Folino <emilfolino@gmail.com>
2023-01-30 15:27:10 +0100

    30452-kmom06

---
+++ menu.py
@@ -0,0 +1,17 @@
+"""
+This module handles logic for the menu.
+"""
+
+
+def print_():
+    """
+    This method prints the menu.
+    """
+    print("lines) Count lines")
+    print("words) Count words")
+    print("letters) Count letters")
+    print("word_frequency) Find 7 most used words")
+    print("letter_frequency) Find 7 most used letters")
+    print("all) Do everything")
+    print("change) Change files")
+    print("q) quit.")
